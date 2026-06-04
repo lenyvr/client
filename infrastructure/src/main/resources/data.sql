4b8ae69fc22ceea5ba893b7abfb4b858b8f8ae05
leni <lenyvr20@gmail.com>
2026-06-04 14:11:04 -0500

    se configura data.sql para que no arroje error insertando el primer registro

--- infrastructure/src/main/resources/data.sql
+++ infrastructure/src/main/resources/data.sql
@@ -1,24 +1,28 @@
 WITH idt_insert AS (
-INSERT INTO identification_type (name, description)
-VALUES ('DNI','Numero de identificacion nacional')
-        RETURNING identification_type_id
+    INSERT INTO identification_type (name, description)
+    VALUES ('DNI', 'Numero de identificacion nacional')
+    ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
+    RETURNING identification_type_id
 ),
 gender_insert AS (
-INSERT INTO gender (name, description)
-VALUES ('F','Femenino')
+    INSERT INTO gender (name, description)
+    VALUES ('F', 'Femenino')
+    ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name -- Lo mismo aquí para obtener el gender_id existente
     RETURNING gender_id
 ),
 person_insert AS (
-INSERT INTO person (first_name, last_name, age, identification_number, address, postal_code, contact_number, identification_type_id)
-SELECT 'Jose', 'Lema', 34, '1234567', 'Otavalo sn y principal', '37890', '098254785'
-        , identification_type_id, gender_id
-FROM idt_insert, gender_insert
+    INSERT INTO person (first_name, last_name, age, identification_number, address, postal_code, contact_number
+                     , identification_type_id, gender_id)
+    SELECT 'Jose', 'Lema', 34, '1234567', 'Otavalo sn y principal', '37890', '098254785'
+          , identification_type_id, gender_id
+    FROM idt_insert, gender_insert
+    ON CONFLICT (identification_number) DO UPDATE SET identification_number = EXCLUDED.identification_number
     RETURNING person_id
 )
 INSERT INTO client (client_code, password, status, person_id)
 SELECT 'C1', '1234', true, person_id
 FROM person_insert
-ON CONFLICT DO NOTHING;
+ON CONFLICT (client_code) DO NOTHING;
 
 INSERT INTO identification_type (name, description)
 VALUES ('NIF','Numero de identificacion foranea')
