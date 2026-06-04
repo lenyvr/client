bd82a9662d8e85990adc991700a04771d7a6f029
leni <lenyvr20@gmail.com>
2026-06-03 20:28:21 -0500

    first commit: create initial project

--- infrastructure/src/main/resources/data.sql
+++ infrastructure/src/main/resources/data.sql
@@ -1,16 +1,35 @@
 WITH idt_insert AS (
 INSERT INTO identification_type (name, description)
 VALUES ('DNI','Numero de identificacion nacional')
-    RETURNING identification_type_id
-    ),
-    person_insert AS (
+        RETURNING identification_type_id
+),
+gender_insert AS (
+INSERT INTO gender (name, description)
+VALUES ('F','Femenino')
+    RETURNING gender_id
+),
+person_insert AS (
 INSERT INTO person (first_name, last_name, age, identification_number, address, postal_code, contact_number, identification_type_id)
 SELECT 'Jose', 'Lema', 34, '1234567', 'Otavalo sn y principal', '37890', '098254785'
-        , identification_type_id
-FROM idt_insert
+        , identification_type_id, gender_id
+FROM idt_insert, gender_insert
     RETURNING person_id
-    )
+)
 INSERT INTO client (client_code, password, status, person_id)
 SELECT 'C1', '1234', true, person_id
 FROM person_insert
 ON CONFLICT DO NOTHING;
+
+INSERT INTO identification_type (name, description)
+VALUES ('NIF','Numero de identificacion foranea')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO identification_type (name, description)
+VALUES ('PPT','Pasaporte')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO gender (name, description)
+VALUES ('M','Masculino')
+ON CONFLICT DO NOTHING;
+
+
--- infrastructure/src/main/resources/schema.sql
+++ infrastructure/src/main/resources/schema.sql
@@ -2,7 +2,16 @@ CREATE TABLE if not exists  public.identification_type (
     identification_type_id serial NOT NULL,
     name varchar(10) NOT NULL,
     description varchar(50) NOT NULL,
-    CONSTRAINT identification_type_unique UNIQUE (identification_type_id)
+    CONSTRAINT identification_type_unique UNIQUE (name),
+    CONSTRAINT identification_type_pk PRIMARY KEY (identification_type_id)
+);
+
+CREATE TABLE if not exists  public.gender (
+    gender_id serial NOT NULL,
+    name varchar(10) NOT NULL,
+    description varchar(50) NOT NULL,
+    CONSTRAINT gender_unique UNIQUE (name),
+    CONSTRAINT gender_pk PRIMARY KEY (gender_id)
 );
 
 CREATE TABLE if not exists public.person (
@@ -10,13 +19,17 @@ CREATE TABLE if not exists public.person (
    first_name varchar(20) NOT NULL,
    last_name varchar(40) NOT NULL,
    age integer NOT NULL,
+   gender_id integer NOT NULL,
    identification_number varchar NOT NULL,
    address varchar(50) NOT NULL,
    postal_code varchar(10) NOT NULL,
    contact_number varchar(15) NOT NULL,
    identification_type_id integer NOT NULL,
-   CONSTRAINT person_unique UNIQUE (person_id),
-   CONSTRAINT person_identification_type_fk FOREIGN KEY (identification_type_id) REFERENCES public.identification_type(identification_type_id)
+   email varchar(50) NULL,
+   CONSTRAINT person_pk PRIMARY KEY (person_id),
+   CONSTRAINT person_identification_unique UNIQUE (identification_number),
+   CONSTRAINT person_identification_type_fk FOREIGN KEY (identification_type_id) REFERENCES public.identification_type(identification_type_id),
+   CONSTRAINT person_gender_fk FOREIGN KEY (gender_id) REFERENCES public.gender(gender_id)
 );
 
 CREATE TABLE if not exists public.client (
